1394a927a4b19670ec9540db685ee344e32752e1
woo <samuel2626@khu.ac.kr>
2025-05-09 18:50:31 +0900

    백엔드 첫 commit

---
+++ khudadak/src/main/java/com/example/khudadak/KhudadakApplication.java
@@ -0,0 +1,13 @@
+package com.example.khudadak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhudadakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhudadakApplication.class, args);
+	}
+
+}
